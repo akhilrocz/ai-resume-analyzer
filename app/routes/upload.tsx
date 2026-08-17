f7436d2cb48e62c76f8fc3179a2e623f86c6fbdd
akhil <b.akhil667@gmail.com>
2026-08-17 15:16:44 +0530

    implement wipe functionality

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -103,7 +103,7 @@ const Upload = () => {
     }
 
     return (
-        <main className="bg-[url('/public/images/bg-main.svg')] bg-cover">
+        <main className="bg-[url('/images/bg-main.svg')] bg-cover">
             <Navbar />
             <section className="main-section">
                 <div className='page-heading py-16'>
